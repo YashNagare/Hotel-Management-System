204509e6c64453f2658026d8f8fbbba22104997b
YashNagare <yashpnagare@gmail.com>
2025-05-05 23:44:41 +0530

    user service communicate with rating service

--- UserService/src/main/java/com/example/userservice/UserService/services/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/example/userservice/UserService/services/impl/UserServiceImpl.java
@@ -1,12 +1,17 @@
 package com.example.userservice.UserService.services.impl;
 
+import com.example.userservice.UserService.entities.Rating;
 import com.example.userservice.UserService.entities.User;
 import com.example.userservice.UserService.exceptions.ResourceNotFoundException;
 import com.example.userservice.UserService.repositiories.UserRepository;
 import com.example.userservice.UserService.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,6 +20,11 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Override
     public User saveUser(User user) {
@@ -30,7 +40,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String userId) {
-        System.out.println("inside serviceImpl" + userId);
-        return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id is not found on server !! : " + userId));
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id is not found on server !! : " + userId));
+
+        ArrayList<Rating> ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/" + user.getUserId(), ArrayList.class);
+        logger.info("{} }", ratingsOfUser);
+
+        user.setRatings(ratingsOfUser);
+
+        return user;
     }
 }
