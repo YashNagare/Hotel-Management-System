b18ae9c02b51f921a51c3423c6d9485762ba554c
YashNagare <yashpnagare@gmail.com>
2025-05-17 22:51:24 +0530

    implemented circuit breaker

--- UserService/src/main/java/com/example/userservice/UserService/controllers/UserController.java
+++ UserService/src/main/java/com/example/userservice/UserService/controllers/UserController.java
@@ -2,6 +2,9 @@ package com.example.userservice.UserService.controllers;
 
 import com.example.userservice.UserService.entities.User;
 import com.example.userservice.UserService.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private UserService userService;
@@ -25,10 +30,24 @@ public class UserController {
 
 //    get single user
     @GetMapping("/{userId}")
+    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId) {
-        System.out.println("inside controller" + userId);
+        logger.info("Get single User Handler: UserController");
         User user = userService.getUser(userId);
         return ResponseEntity.ok(user);
+    }
+
+//    creating fall back method for circuit breaker
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex) {
+        logger.info("Fallback is executed because service is down : ", ex.getMessage());
+        User user = User
+                .builder()
+                .email("dummy@gmail.com")
+                .name("Dummy")
+                .about("This user is created dummy because some service is down")
+                .userId("141234")
+                .build();
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
 //    get all users
